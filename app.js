17415d40d24c4dee0fe45466ae1bf354433da75c
ArashDerArcher <aj.javanmardi@gmail.com>
2018-12-22 19:23:15 +0330

    express

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('passport');
 
+
+
+// Init App
+const app = express();
+
+// Load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// set Public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
