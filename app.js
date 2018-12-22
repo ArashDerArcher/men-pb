aabf5b5bf40090d9379bce0e523d9596b928c004
cell <cell@localhost.localdomain>
2018-12-22 19:00:32 +0330

    require

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('passport');
+
